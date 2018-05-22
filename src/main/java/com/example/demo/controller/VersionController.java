a9e9bad8539323630e04938212519b28aa458f42
Jimmy Huang <huangji2@oocl.com>
2018-05-22 16:03:27 +0800

    fix code smell

--- src/main/java/com/example/demo/controller/VersionController.java
+++ src/main/java/com/example/demo/controller/VersionController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
--- src/main/java/com/example/demo/domain/Fibonacci.java
+++ src/main/java/com/example/demo/domain/Fibonacci.java
@@ -2,6 +2,10 @@ package com.example.demo.domain;
 
 public class Fibonacci {
 
+    private Fibonacci() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static int of(int num) {
         if(num > 2) {
             return of(num - 2) + of(num - 1);
